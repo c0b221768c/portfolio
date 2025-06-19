3269b4cfa9d99f5dc779bec7556a6b7142ae5752
c0b221768c <c0b227168c@edu.teu.ac.jp>
2025-06-20 00:02:01 +0900

    初期ページ作成

--- workspace/app/ToDay/page.tsx
+++ workspace/app/ToDay/page.tsx
@@ -5,16 +5,14 @@ const styles = tv({
   slots: {
     base: 'min-h-screen flex flex-col items-center justify-center gap-10',
     title: 'text-2xl font-bold',
-    link: 'hover:text-blue-400'
   }
 })
 
 export default function Home() {
-  const {base, title, link} = styles()
+  const {base, title} = styles()
   return (
     <main className={base()}>
-      <h1 className={title()}>C0B221768c's PORTFOLIO page</h1>
-      <Link href='/ToDay' className={link()}>Go to ToDay app</Link>
+      <h1 className={title()}>ToDay app</h1>
     </main>
   )
 }
--- workspace/app/layout.tsx
+++ workspace/app/layout.tsx
@@ -1,19 +1,23 @@
 import '@/styles/globals.css'
 
+import {tv} from 'tailwind-variants'
 import {Inter} from 'next/font/google'
 import { ThemeProvider } from 'next-themes'
 
 const inter = Inter({ subsets: ["latin"]})
 
-export const metadata = {
-  title: "MY PORTFOLIO",
-  description: "Portfolio Site"
-}
+export const styles = tv({
+	slots: {
+		font: `${inter.className}`,
+		base: 'min-h-screen bg-neutral-100 text-neutral-900 antialiased dark:bg-neutral-900 dark:text-neutral-100'
+	}
+})
 
 export default function Layout({children}: {children:React.ReactNode}){
+	const {font, base} = styles()
   return (
-    <html lang='en' className={`${inter.className}`} suppressContentEditableWarning>
-      <body>
+    <html lang='en' className={font()} suppressContentEditableWarning>
+      <body className={base()}>
 				<ThemeProvider
 					attribute="class"
 					defaultTheme="system"
